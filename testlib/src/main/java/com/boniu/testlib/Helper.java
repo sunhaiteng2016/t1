15ce6d12dcd984a18a0d426c0dc7ac2cbe1b9687
Boniu <>
2020-06-03 10:17:46 +0800

    第一次 提交

---
+++ testlib/src/main/java/com/boniu/testlib/Helper.java
@@ -0,0 +1,19 @@
+package com.boniu.testlib;
+
+import android.content.Context;
+
+import com.bun.miitmdid.core.JLibrary;
+
+public class Helper {
+
+    public static  void init(Context context){
+        try {
+            JLibrary.InitEntry(context);
+
+
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
